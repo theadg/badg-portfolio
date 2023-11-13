21beb8294d200da4e5996c9dc33f847695b8c558
theadg <deguzmanbernardandrew@gmail.com>
2023-11-14 04:52:16 +0800

    Update base

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/badg-portfolio",
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
