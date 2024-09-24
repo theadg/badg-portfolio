46664f0326afc3f0d8c2d274829f0bf9bf6a8234
Bernard De Guzman <deguzmanbernardandrew@gmail.com>
2024-09-24 18:18:36 +0800

    feat: add additional tech

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,35 +1,33 @@
 <template>
-  <div class="max-w-7xl m-center pt-48" id="skills">
+  <div class="pt-48 max-w-7xl m-center" id="skills">
     <div
-      class="skills text-center mt-10"
+      class="mt-10 text-center skills"
       data-aos="fade-up"
       data-aos-delay="100"
       data-aos-duration="800"
     >
       <h2 class="text-5xl dark:text-white">Skills</h2>
-      <span class="f-gray text-sm dark:text-white">My Technical Level</span>
+      <span class="text-sm f-gray dark:text-white">My Technical Level</span>
     </div>
-    <div class="skill-details flex mt-20 flex-col lg:flex-row lg:gap-10">
+    <div class="flex flex-col mt-20 skill-details lg:flex-row lg:gap-10">
       <div
         data-aos="fade-down"
         data-aos-duration="1000"
         data-aos-delay="200"
-        class="lg:w-1/3 md:w-96 md:m-auto md:mb-16 card bg-white drop-shadow-2xl dark:bg-slate-800"
+        class="bg-white lg:w-1/3 md:w-96 md:m-auto md:mb-16 card drop-shadow-2xl dark:bg-slate-800"
       >
-        <h2 class="pt-7 pl-5 flex items-center">
+        <h2 class="flex items-center pl-5 pt-7">
           <font-awesome-icon
-            class="fa-2xl pr-3 f-blue w-20"
+            class="w-20 pr-3 fa-2xl f-blue"
             :icon="['fas', 'code']"
           />
           <div class="flex flex-col">
             <h2 class="text-xl dark:text-white">Frontend</h2>
-            <span class="text-sm f-gray dark:text-white"
-              >Technologies</span
-            >
+            <span class="text-sm f-gray dark:text-white">Technologies</span>
           </div>
         </h2>
         <div
-          class="description grid grid-cols-4 gap-4 m-auto lg:px-10 px-5 pt-10"
+          class="grid grid-cols-4 gap-4 px-5 pt-10 m-auto description lg:px-10"
         >
           <div class="p-1 card-mini">
             <img
@@ -117,11 +115,11 @@
           </div>
           <div class="p-1 card-mini">
             <img
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/120px-CSS3_logo_and_wordmark.svg.png"
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/2048px-Typescript_logo_2020.svg.png"
               class="h-14"
               alt="CSS"
             />
-            <span class="dark:text-white">CSS</span>
+            <span class="dark:text-white">TypeScript</span>
           </div>
           <div class="p-1 card-mini">
             <img
@@ -137,22 +135,20 @@
         data-aos="fade-down"
         data-aos-duration="1000"
         data-aos-delay="400"
-        class="lg:w-1/3 md:w-96 md:m-auto md:mb-16 card bg-white drop-shadow-2xl dark:bg-slate-800"
+        class="bg-white lg:w-1/3 md:w-96 md:m-auto md:mb-16 card drop-shadow-2xl dark:bg-slate-800"
       >
-        <h2 class="pt-7 pl-5 flex items-center">
+        <h2 class="flex items-center pl-5 pt-7">
           <font-awesome-icon
-            class="fa-2xl pr-3 f-blue w-20"
+            class="w-20 pr-3 fa-2xl f-blue"
             :icon="['fas', 'code-branch']"
           />
           <div class="flex flex-col">
             <h2 class="text-xl dark:text-white">Backend</h2>
-            <span class="text-sm f-gray dark:text-white"
-              >Techonolgoies</span
-            >
+            <span class="text-sm f-gray dark:text-white">Techonolgoies</span>
           </div>
         </h2>
         <div
-          class="description grid grid-cols-4 gap-4 m-auto lg:px-10 px-5 pt-10"
+          class="grid grid-cols-4 gap-4 px-5 pt-10 m-auto description lg:px-10"
         >
           <div class="p-1 card-mini">
             <img
@@ -161,7 +157,8 @@
               alt="Laravel"
             />
             <span class="dark:text-white">Laravel</span>
-          </div>          <div class="p-1 card-mini">
+          </div>
+          <div class="p-1 card-mini">
             <img
               src="https://seeklogo.com/images/M/mongodb-logo-D13D67C930-seeklogo.com.png"
               alt="AWS"
@@ -198,7 +195,32 @@
               class="h-14"
               alt="Mysql"
             />
-            <span class="dark:text-white">Mysql</span>
+            <span class="dark:text-white">MySQL</span>
+          </div>
+          <div class="p-1 card-mini">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Postgresql_elephant.svg/640px-Postgresql_elephant.svg.png"
+              class="h-14"
+              alt="Mysql"
+            />
+            <span class="dark:text-white">PostgreSQL</span>
+          </div>
+          <div class="p-1 card-mini">
+            <img
+              src="https://cdn.worldvectorlogo.com/logos/prisma-4.svg"
+              class="h-14"
+              alt="CodeIgniter"
+            />
+            <span class="dark:text-white">Prisma</span>
+          </div>
+
+          <div class="p-1 card-mini">
+            <img
+              src="https://logowik.com/content/uploads/images/supabase-icon1721342077.logowik.com.webp"
+              class="h-14"
+              alt="CodeIgniter"
+            />
+            <span class="dark:text-white">Supabase</span>
           </div>
 
           <div class="p-1 card-mini">
@@ -215,11 +237,11 @@
         data-aos="fade-down"
         data-aos-duration="1000"
         data-aos-delay="600"
-        class="lg:w-1/3 md:w-96 md:m-auto md:mb-16 card bg-white drop-shadow-2xl dark:bg-slate-800"
+        class="bg-white lg:w-1/3 md:w-96 md:m-auto md:mb-16 card drop-shadow-2xl dark:bg-slate-800"
       >
-        <h2 class="pt-7 pl-5 flex items-center">
+        <h2 class="flex items-center pl-5 pt-7">
           <font-awesome-icon
-            class="fa-2xl pr-3 f-blue w-20"
+            class="w-20 pr-3 fa-2xl f-blue"
             :icon="['fas', 'brain']"
           />
           <div class="flex flex-col">
@@ -228,7 +250,7 @@
           </div>
         </h2>
         <div
-          class="description grid grid-cols-4 gap-4 m-auto lg:px-10 px-5 pt-10"
+          class="grid grid-cols-4 gap-4 px-5 pt-10 m-auto description lg:px-10"
         >
           <div class="p-1 card-mini">
             <img
